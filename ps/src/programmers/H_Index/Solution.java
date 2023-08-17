d964483f5dc77aafbcd7efd247a420dfb2589f32
devjun10 <devjun1023@gmail.com>
2023-08-17 16:10:03 +0900

    ps: H-Index
    - 정렬
    - 문제 링크(https://school.programmers.co.kr/learn/courses/30/lessons/42747)

---
+++ ps/src/programmers/H_Index/Solution.java
@@ -0,0 +1,21 @@
+package programmers.H_Index;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public int solution(int[] citations) {
+        Arrays.sort(citations);
+
+        int length = citations.length;
+
+        int answer = 0;
+        for (int index = 0; index < length; index++) {
+            if (citations[index] >= length - index) {
+                answer = length - index;
+                break;
+            }
+        }
+        return answer;
+    }
+}
