5453209e658e7e2158084dd12110f3650710aa59
devjun10 <devjun1023@gmail.com>
2023-08-19 14:26:20 +0900

    ps: 방문길이(r)
    - BFS
    - 문제 링크(https://school.programmers.co.kr/learn/courses/30/lessons/49994?language=java)

--- ps/src/programmers/방문_길이/Solution.java
+++ ps/src/programmers/방문_길이/Solution.java
@@ -1,72 +1,76 @@
 package programmers.방문_길이;
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Queue;
+import java.util.Set;
 
 public class Solution {
 
-    private static int[] dx = {1, -1, 0, 0};
+    private static Set<Point> histories = new HashSet<>();
+    private static Map<String, Integer> directions = new HashMap<>();
+    private static int[] dx = {1, -1, 0, 0};    // 남 북 동 서
     private static int[] dy = {0, 0, 1, -1};
-    private static Map<String, Integer> direction = new HashMap<>();
-    private static List<History> historyList = new ArrayList<>();
 
     static {
-        direction.put("R", 2);
-        direction.put("L", 3);
-        direction.put("D", 0);
-        direction.put("U", 1);
+        directions.put("R", 2);
+        directions.put("L", 3);
+        directions.put("D", 0);
+        directions.put("U", 1);
     }
 
-    public static int solution(String dirs) {
-        int answer = 0;
+    public int solution(String dirs) {
         Queue<String> commands = new LinkedList<>();
-
-        for (int index = 0; index < dirs.length(); index++) {
-            commands.add(String.valueOf(dirs.charAt(index)));
+        String[] temp = dirs.split("");
+        for (String command : temp) {
+            commands.add(command);
         }
 
         Queue<Point> queue = new LinkedList<>();
         queue.add(new Point(5, 5, 5, 5, 0));
-
+        int answer = 0;
         while (!commands.isEmpty()) {
-            String command = commands.poll();
             Point point = queue.poll();
             int x = point.nextX;
             int y = point.nextY;
 
-            int nextX = x + dx[direction.get(command)];
-            int nextY = y + dy[direction.get(command)];
-            History history = new History(nextX, nextY, x, y);
+            String command = commands.poll();
+            int direction = directions.get(command);
 
-            if (moveable(nextX, nextY) && !hasHistory(history)) {
-                queue.add(new Point(nextX, nextY, x, y, point.count + 1));
-                historyList.add(history);
-                historyList.add(new History(x, y, nextX, nextY));
+            int nextX = x + dx[direction];
+            int nextY = y + dy[direction];
+
+            if (!moveable(nextX, nextY)) {
+                queue.add(point);
+                continue;
+            }
+            Point pointHistory = new Point(nextX, nextY, x, y);
+            if (!has(pointHistory)) {
+                Point nextPoint = new Point(nextX, nextY, x, y, point.count + 1);
+                Point reversePoint = new Point(x, y, nextX, nextY, point.count + 1);
+                histories.add(nextPoint);
+                histories.add(reversePoint);
+                queue.add(nextPoint);
                 answer = point.count + 1;
                 continue;
             }
-            if (moveable(nextX, nextY) && hasHistory(history)) {
-                queue.add(new Point(nextX, nextY, x, y, point.count));
-                continue;
-            }
-            if (!moveable(nextX, nextY)) {
-                queue.add(point);
+            if (has(pointHistory)) {
+                Point nextPoint = new Point(nextX, nextY, x, y, point.count);
+                queue.add(nextPoint);
             }
         }
         return answer;
     }
 
-    static boolean moveable(int x, int y) {
+    private boolean moveable(int x, int y) {
         return x >= 0 && x < 11 && y >= 0 && y < 11;
     }
 
-    static boolean hasHistory(History history) {
-        return historyList.contains(history);
+    private boolean has(Point point) {
+        return histories.contains(point);
     }
 
     static class Point {
@@ -77,6 +81,13 @@ public class Solution {
         int beforeY;
         int count;
 
+        public Point(int nextX, int nextY, int beforeX, int beforeY) {
+            this.nextX = nextX;
+            this.nextY = nextY;
+            this.beforeX = beforeX;
+            this.beforeY = beforeY;
+        }
+
         public Point(int nextX, int nextY, int beforeX, int beforeY, int count) {
             this.nextX = nextX;
             this.nextY = nextY;
@@ -84,33 +95,18 @@ public class Solution {
             this.beforeY = beforeY;
             this.count = count;
         }
-    }
-
-    static class History {
-
-        int nextX;
-        int nextY;
-        int beforeX;
-        int beforeY;
-
-        public History(int nextX, int nextY, int beforeX, int beforeY) {
-            this.nextX = nextX;
-            this.nextY = nextY;
-            this.beforeX = beforeX;
-            this.beforeY = beforeY;
-        }
 
         @Override
         public boolean equals(Object o) {
             if (this == o) {
                 return true;
             }
-            if (!(o instanceof History)) {
+            if (!(o instanceof Point)) {
                 return false;
             }
-            History history = (History) o;
-            return nextX == history.nextX && nextY == history.nextY && beforeX == history.beforeX
-                && beforeY == history.beforeY;
+            Point point = (Point) o;
+            return nextX == point.nextX && nextY == point.nextY && beforeX == point.beforeX
+                && beforeY == point.beforeY;
         }
 
         @Override
