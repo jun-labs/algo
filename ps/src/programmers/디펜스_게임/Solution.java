bd4efbef53749936868cef2ecd1d683b0a7f2cf2
devjun10 <devjun1023@gmail.com>
2023-08-21 12:14:45 +0900

    ps: 디펜스 게임(r)
    - 큐
    - 문제 링크(https://school.programmers.co.kr/learn/courses/30/lessons/142085)

--- ps/src/programmers/디펜스_게임/Solution.java
+++ ps/src/programmers/디펜스_게임/Solution.java
@@ -3,19 +3,17 @@ package programmers.디펜스_게임;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
-import static java.util.Collections.reverseOrder;
-
 public class Solution {
+
+    private final Queue<Integer> queue = new PriorityQueue<>();
 
     public int solution(int n, int k, int[] enemy) {
         int length = enemy.length;
 
-        Queue<Integer> queue = new PriorityQueue<>(reverseOrder());
-
         for (int index = 0; index < length; index++) {
             int count = enemy[index];
             n -= count;
-            queue.add(count);
+            queue.add(enemy[index]);
             if (n < 0) {
                 if (k >= 1 && !queue.isEmpty()) {
                     k -= 1;
