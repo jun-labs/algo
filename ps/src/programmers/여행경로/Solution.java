6aca66edd776207691f9c556515b327eece49ab6
devjun10 <devjun1023@gmail.com>
2023-08-27 00:34:35 +0900

    ps: 여행경로(r)
    - DFS
    - 문제 링크(https://school.programmers.co.kr/learn/courses/30/lessons/43164?language=java)

--- ps/src/programmers/여행경로/Solution.java
+++ ps/src/programmers/여행경로/Solution.java
@@ -8,30 +8,28 @@ class Solution {
 
     private boolean[] visited;
     private List<String> allRoute;
+    private String[][] tickets;
 
     public String[] solution(String[][] tickets) {
-        String[] answer = {};
-        int count = 0;
+        this.tickets = tickets;
         visited = new boolean[tickets.length];
         allRoute = new ArrayList<>();
 
-        dfs("ICN", "ICN", tickets, count);
-
+        dfs("ICN", "ICN", 0);
         Collections.sort(allRoute);
-        answer = allRoute.get(0).split(" ");
-        return answer;
+        return allRoute.get(0).split(" ");
     }
 
-    public void dfs(String start, String route, String[][] tickets, int count) {
+    private void dfs(String start, String route, int count) {
         if (count == tickets.length) {
             allRoute.add(route);
             return;
         }
-
         for (int index = 0; index < tickets.length; index++) {
             if (start.equals(tickets[index][0]) && !visited[index]) {
                 visited[index] = true;
-                dfs(tickets[index][1], route + " " + tickets[index][1], tickets, count + 1);
+                String next = tickets[index][1];
+                dfs(next, route + " " + next, count + 1);
                 visited[index] = false;
             }
         }
