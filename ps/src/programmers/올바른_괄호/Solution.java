23dcd54b69ea3200dc24b10065891b0f8c74df19
devjun10 <devjun1023@gmail.com>
2023-09-13 01:19:01 +0900

    ps: 올바른 괄호(r)
    - 스택
    - 문제 링크(https://school.programmers.co.kr/learn/courses/30/lessons/12909)

--- ps/src/programmers/올바른_괄호/Solution.java
+++ ps/src/programmers/올바른_괄호/Solution.java
@@ -1,29 +1,35 @@
 package programmers.올바른_괄호;
 
+import java.util.Stack;
+
 public class Solution {
 
     private char startBracket = '(';
 
     public boolean solution(String s) {
-        int startBracketCount = 0;
-        int endBracketCount = 0;
-
         if (s.length() == 0) {
             return false;
         }
+        if (s.length() % 2 != 0) {
+            return false;
+        }
 
+        Stack<Character> stack = new Stack<>();
         for (int index = 0; index < s.length(); index++) {
             char bracket = s.charAt(index);
-            if (bracket == startBracket) {
-                startBracketCount++;
-            } else {
-                endBracketCount++;
-            }
 
-            if (endBracketCount > startBracketCount) {
-                return false;
+            if (bracket == startBracket) {
+                stack.push(bracket);
+            } else {
+                if (!stack.isEmpty()) {
+                    if (stack.peek() == '(') {
+                        stack.pop();
+                    }
+                } else {
+                    stack.push(bracket);
+                }
             }
         }
-        return startBracketCount == endBracketCount;
+        return stack.isEmpty();
     }
 }
